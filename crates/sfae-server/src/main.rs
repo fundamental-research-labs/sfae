4a7fe15c1d406b79c1c297a24cc93d5df52ed5ad
Gaëtan de Villèle <gdevillele@gmail.com>
2026-04-06 13:43:15 -0700

    include tower_http in default log filter for request tracing

--- crates/sfae-server/src/main.rs
+++ crates/sfae-server/src/main.rs
@@ -465,7 +465,7 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "sfae_server=info".parse().unwrap()),
+                .unwrap_or_else(|_| "sfae_server=info,tower_http=debug".parse().unwrap()),
         )
         .init();
 
